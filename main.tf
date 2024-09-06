6b51490e0d7a892e959fe1a3f4d3acb6c2451c61
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2024-09-06 13:53:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,25 +12,29 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
 }
 
-
-data "aws_ami" "ecs_ami" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ecs_ami.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {
     Name                 = var.instance_name
-    "Linux Distribution" = "amazonlinux"
+    "Linux Distribution" = "Ubuntu"
     "test" = "even"
   }
 }
